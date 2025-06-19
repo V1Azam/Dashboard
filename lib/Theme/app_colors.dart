7c92c6834d76b65bf4c7c5154924848a74742a41
Azam Aminuddin <v1.0azam@gmail.com>
2025-06-19 10:17:46 +0800

    Added Theme file to standardise colours

---
+++ lib/Theme/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryGreen = Color(0xFFE8F5E9);
+  static const Color mainBackground = Color(0xFFF5F5F5);
+  static const Color cardBackground = Colors.white;
+  static const Color accentGreen = Color(0xFF4CAF50);
+  static const Color accentBlue = Color(0xFF2196F3);
+  static const Color iconInnerWhite = Colors.white;
+  static const Color comingSoon = Color(0xFFFF5252);
+  static const Color textDark = Color(0xFF333333);
+  static const Color textLight = Colors.white;
+}
